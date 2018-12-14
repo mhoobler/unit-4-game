b118c9238bf51b301cdefb407d7c2d5aad8766ce
Michael Hoobler <hoobler.michael@gmail.com>
2018-12-14 16:13:09 -0500

    added win state

--- crystal-collector/object-test.js
+++ crystal-collector/object-test.js
@@ -3,30 +3,58 @@ var game = {
 	target_number: 0,
 	current_total: 0,
 	score: 0,
-	game_running: false,
+	number_of_guesses: 0,
 	crystal_array: [],
 
-	startGame: function() {
+	startGame: function(game_state) {
 		this.createTargetNumber();
 		this.current_total = 0;
-		this.game_running = true;
-		crystalSetup();
-		htmlSetup();
-		console.log("ready");
+		this.number_of_guesses = 0;
+		this.crystal_array = [];
+
+		//if player just won
+		if(game_state === "WIN"){
+			crystalSetup();
+			htmlSetup();
+			console.log("ready");
+
+		//if player just lossed
+		} else if(game_state === "LOSS") {
+			this.score = 0;
+
+		//if game just started
+		} else {
+			crystalSetup();
+			htmlSetup();
+			console.log("game started");
+		}
 	},
 
 	createTargetNumber: function() {
-		this.target_number = Math.ceil(Math.random()*4) * Math.ceil(Math.random()*10) + 32;
+		this.target_number = Math.ceil(Math.ceil(Math.random()*30) + 60);
 		$("#target-number").text(this.target_number);
 	},
 
 	checkWin: function() {
 		if(this.current_total === this.target_number) {
+			this.addScore();
 			alert("You Win!");
+			this.startGame("WIN");
 		} else if (this.current_total > this.target_number) {
 			alert("You lose :(");
+			game.startGame("LOSS");
+		};
+	},
+
+	addScore: function() {
+		if (this.number_of_guesses < 15) {
+			this.score += 15 - this.number_of_guesses;
+		} else {
+			this.score++;
 		};
 	}
+
+
 };
 
 function crystal(color, value) {
@@ -46,7 +74,7 @@ var colors = ["red", "green", "blue", "yellow"];
 
 function crystalSetup() {
 	for(var i=0; i<colors.length; i++){
-		var random = Math.ceil(Math.random() * 14) + 3;
+		var random = Math.ceil(Math.random() * 7) + 3;
 		var x = new crystal(colors[i], random);
 		game.crystal_array.push(x);
 	}
@@ -54,6 +82,11 @@ function crystalSetup() {
 }
 
 function htmlSetup() {
+	
+	$("#current-score").text(game.score);
+	$("#current-total").text(0);
+
+	$("#crystal-row").empty();
 	for(i=0; i<game.crystal_array.length; i++){
 		var crystal_element = $("<div>");
 		crystal_element.addClass("col-md-3 crystal");
@@ -67,9 +100,20 @@ function htmlSetup() {
 
 game.startGame();
 
-$(".crystal").click(function () {
+$(document).on("click", ".crystal", function () {
 	console.log($(this).attr("value"));
 	game.current_total += parseFloat($(this).attr("value"));
+	game.number_of_guesses++;
+	console.log(game.number_of_guesses);
+	$("#current-total").text(game.current_total);
+	game.checkWin();
+});
+
+$("#cheat").click(function() {
+	console.log($(this).attr("value"));
+	game.current_total = game.target_number;
+	game.number_of_guesses++;
+	console.log(game.number_of_guesses);
 	$("#current-total").text(game.current_total);
 	game.checkWin();
 });
